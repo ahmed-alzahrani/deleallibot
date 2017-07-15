1d11d6f449fed33516e83f5a6ae42ea1afcebc79
Ahmed Al-Zahrani <ahmedalzahrani94@gmail.com>
2017-07-15 17:49:31 -0400

    added archiving to prevent duplicate commenting

--- DeleAlli.py
+++ DeleAlli.py
@@ -1,4 +1,5 @@
 import praw
+import os
 from random import randint # to serve a random gif from the gif file
 import time
 import config #read the config file
@@ -18,16 +19,35 @@ def all_casings(name):
                 yield first.lower() + sub_casing
                 yield first.upper() + sub_casing
 
+def appendArchive(comment, archive):
+    archive.append(comment.id)
+
+    with open ("archive.txt", "a") as f:
+        f.write(comment.id + "\n")
 
 def botAuthenticate():
     reddit = praw.Reddit('authenticate', user_agent = config.user_agent)
     print ('Authenticated as {}'.format(reddit.user.me()))
     return reddit
 
+def doIreply(comment, archive, reddit):
+    # return comment.id not in archive and comment.author != reddit.user.me()
+    return comment.id not in archive
+
 def getAllCasings(lookFor, allCasings):
     for string in lookFor:
         allCasings += list(all_casings(string))
     return allCasings
+
+def getArchivedComments():
+    if not os.path.isfile("archive.txt"):
+        archive = []
+
+    else:
+        with open("archive.txt", "r") as f:
+            archive = f.read().split("\n")
+
+    return archive
 
 
 def grabRandomGif():
@@ -35,22 +55,24 @@ def grabRandomGif():
     gif_path = config.routes[randint(0, size)]
     return gif_path
 
-def reply(reddit, subreddit, limit, casings):
+def reply(reddit, subreddit, limit, casings, archive):
     for comment in reddit.subreddit(subreddit).comments(limit=limit):
         for casing in casings:
-            if casing in comment.body:
+            if casing in comment.body and doIreply(comment, archive, reddit):
                 print ('found ' + casing + ' in comment ' + comment.id + ' ' + comment.body)
                 gif_path = grabRandomGif()
-                comment.reply('[hi]({})'.format(gif_path))
+                comment.reply('[Did someone say Dele Alli?]({})'.format(gif_path))
                 print ('responded to a comment!')
+                appendArchive(comment, archive)
     print("Sleeping for 10 seconds....")
     time.sleep(10)
 
 def main():
     reddit = botAuthenticate()
+    archive = getArchivedComments()
     allCasings = getAllCasings(config.lookFor, [])
     while True:
-        reply(reddit, config.subreddit, config.limit, allCasings)
+        reply(reddit, config.subreddit, config.limit, allCasings, archive)
 
 if __name__ == '__main__':
     main()
